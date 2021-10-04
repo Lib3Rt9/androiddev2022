d67418bd00b590b5dc5ac2a499ff8211b26bfae5
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2021-10-04 22:29:02 +0700

    Lap1

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,53 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment ff = ForecastFragment.newInstance("", "");
+
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+            R.id.container, ff).commit();
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Weather", "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Weather", "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather", "onSDestroy() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Weather", "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Weather", "onResume() called");
+    }
+
+    
+}

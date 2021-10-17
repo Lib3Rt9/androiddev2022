449c52590ba2246cafcf893c83216aa6b5a6b439
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2021-10-17 13:02:18 +0700

    done Prac 7

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -2,6 +2,11 @@ package vn.edu.usth.weather;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -13,21 +18,34 @@ public class WeatherActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+/*
 
         ForecastFragment FcFr = ForecastFragment.newInstance("", "");
         WeatherFragment WtFr = WeatherFragment.newInstance("", "");
 
         // Add the fragment to the 'container' FrameLayout
-        /*
+        */
+/*
         getSupportFragmentManager().beginTransaction().add(
                 R.id.container, FcFr).commit();
-                */
+                *//*
+
 
         getSupportFragmentManager().beginTransaction().add(
                 R.id.frTag, WtFr).commit();
 
         getSupportFragmentManager().beginTransaction().add(
                 R.id.fcFr, FcFr).commit();
+*/
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        Log.i("Weather", "onCreate()");
+
     }
     
     @Override
@@ -60,5 +78,34 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i("Weather", "onResume() called");
     }
 
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+        @Override
+        public Fragment getItem(int page) {
+// returns an instance of Fragment corresponding to the specified page
+            switch (page) {
+                case 0:
+                    return new WeatherAndForecastFragment();
+                case 1:
+                    return new WeatherAndForecastFragment();
+                case 2:
+                    return new WeatherAndForecastFragment();
+            }
+            return new Fragment(); // failsafe
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
+// returns a tab title corresponding to the specified page
+            return titles[page];
+        }
+    }
     
 }
